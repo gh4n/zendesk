8b3d8fc8694396be73cd10d84e9f98c76a90da5f
Grace <hgrace503@gmail.com>
2019-07-08 16:47:56 +1000

    "Mocked up unittests"

---
+++ tests/test_entry.py
@@ -0,0 +1,41 @@
+import context
+from query import query
+
+class TestData:
+
+    def __init__(self, test_data_file):
+        self.user_data_missing_org_id
+        self.user_data_
+        self.entry = self.load(test_data_file)
+
+class TestQuery(unittest.TestCase):
+
+    def test_get_field(self):
+        return 
+    
+    def test_create_query(self):
+        return
+
+    def _test_create_query_missing_keys(self):
+        return        
+    
+    def test_create_query_user(self):
+        return
+    
+    def test_create_query_orgs(self):
+        return
+    
+    def test_create_query_tickets(self):
+        return 
+
+    def test_get_related_queries(self):
+        return
+    
+    def test_get_related_queries_missing_keys(self)::
+        return
+
+    def test_save_related_results(self):
+        return
+    
+    def test_format(self):
+        return
